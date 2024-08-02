bd916566bdfbac1f96e69705da524a2944401dc1
Joe Corall <jjc223@lehigh.edu>
2024-08-02 08:46:38 -0400

    Remove cache-warmer query param from facets

---
+++ src/EventSubscriber/FacetQueryStringSubscriber.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\lehigh_islandora\EventSubscriber;
+
+use Drupal\facets\Event\QueryStringCreated;
+use Drupal\facets\Event\FacetsEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class FacetQueryStringSubscriber implements EventSubscriberInterface {
+
+  /**
+   * Remove our custom query parameter if it exists.
+   *
+   * @param \Drupal\facets\Event\QueryStringCreated $event
+   *   The event to process.
+   */
+  public function onQueryStringCreated(QueryStringCreated $event) {
+    $params = $event->getQueryParameters();
+    if (!$params->get('cache-warmer')) {
+      return;
+    }
+
+    $params->remove('cache-warmer');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[FacetsEvents::QUERY_STRING_CREATED][] = ['onQueryStringCreated'];
+    return $events;
+  }
+}
