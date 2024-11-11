02dd413d928564b652c6d871e2c4883bf837bfa5
Joe Corall <jjc223@lehigh.edu>
2024-11-11 08:30:08 -0500

    Avoid Drupal behaviours for speedier execution

--- js/node.js
+++ js/node.js
@@ -1,25 +1,19 @@
-(function ($, Drupal) {
-  Drupal.behaviors.lehighIslandoraNode = {
-    attach: function (context, settings) {
-      $(once('access-request', 'a[data-bs-target="#accessRequestModal"]', context)).one('click', function (e) {
-        $.ajax({
-            url: '/ajax/access-request-form',
-            type: 'GET',
-            success: function (response) {
-              var script = document.createElement('script');
-              script.type = 'text/javascript';
-              script.src = 'https://challenges.cloudflare.com/turnstile/v0/api.js';
-              script.onload = function() {
-                $('#access-request-form-container').html(response.form);
-                Drupal.attachBehaviors();
-              }
-              document.head.appendChild(script);
-            },
-            error: function () {
-                console.error('Failed to load access request form.');
-            }
-        });
-      });
-    }
-  }
-})(jQuery, Drupal)
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll('a[data-bs-target="#accessRequestModal"]').forEach(function (element) {
+    element.addEventListener("click", function (e) {
+      fetch('/ajax/access-request-form')
+        .then(response => response.json())
+        .then(data => {
+          var script = document.createElement('script');
+          script.type = 'text/javascript';
+          script.src = 'https://challenges.cloudflare.com/turnstile/v0/api.js';
+          script.onload = function () {
+            document.getElementById('access-request-form-container').innerHTML = data.form;
+            Drupal.attachBehaviors();
+          };
+          document.head.appendChild(script);
+        })
+        .catch(() => console.error('Failed to load access request form.'));
+    }, { once: true });
+  });
+});
