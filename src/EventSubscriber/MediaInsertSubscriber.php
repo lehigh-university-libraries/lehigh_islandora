7ea385feb8bd43d850513d3f651e8f3509efc102
Joe Corall <jjc223@lehigh.edu>
2024-11-20 07:25:55 -0500

    Code cleanup and add aggregated PDF action trigger

--- src/EventSubscriber/MediaInsertSubscriber.php
+++ src/EventSubscriber/MediaInsertSubscriber.php
@@ -39,42 +39,85 @@ class MediaInsertSubscriber implements EventSubscriberInterface {
       return;
     }
 
+    if (!in_array($media->bundle(), ['document', 'file', 'image'])) {
+      return;
+    }
+
     $action_name = '';
     foreach ($media->field_media_of as $field_media_of) {
+      if (is_null($field_media_of->entity)) {
+        continue;
+      }
+
       $node = $field_media_of->entity;
-      // check if microsoft file needs PDF
-      if ($media->bundle() === 'file' || $media->bundle() === 'document') {
-        switch($media->field_media_use->entity->field_external_uri->uri) {
-          case 'http://pcdm.org/use#PreservationMasterFile':
-            $action_name = 'microsoft_document_to_pdf';
-            break;
-          case 'http://pcdm.org/use#OriginalFile':
-            if ($node->hasField('field_add_coverpage') &&
-              !$node->field_add_coverpage->isEmpty() &&
-              $node->field_add_coverpage->value &&
-              $media->bundle() == 'document') {
-              $action_name = 'digital_document_add_coverpage';
-            }
-            break;
+      if ($media->field_media_use->entity->field_external_uri->uri === 'http://vocab.getty.edu/page/aat/300027363') {
+        // todo: if zip, parse directory tree and create manifest
+      }
+      elseif ($media->field_media_use->entity->field_external_uri->uri === 'http://pcdm.org/use#PreservationMasterFile') {
+        $action_name = 'microsoft_document_to_pdf';
+      }
+      elseif ($media->field_media_use->entity->field_external_uri->uri === 'http://pcdm.org/use#OriginalFile') {
+        if ($node->hasField('field_add_coverpage') &&
+          !$node->field_add_coverpage->isEmpty() &&
+          $node->field_add_coverpage->value &&
+          $media->bundle() == 'document') {
+          $action_name = 'digital_document_add_coverpage';
+        }
+        // todo: if zip, parse directory tree and create manifest
+      }
+      elseif ($media->field_media_use->entity->field_external_uri->uri === 'http://pcdm.org/use#ServiceFile') {
+        // bail if the parent node is not a page
+        if (!$node->hasField('field_model') ||
+          $node->field_model->isEmpty() ||
+          is_null($node->field_model->entity) ||
+          $node->field_model->entity->field_external_uri->uri !== 'http://id.loc.gov/ontologies/bibframe/part') {
+          continue;
+        }
+
+        foreach ($node->field_member_of as $parent) {
+          if (is_null($parent->entity)) {
+            continue;
+          }
+
+          // See if all service files have been created
+          // for the parent paged content item.
+          $readyToAggregate = \Drupal::database()->query('SELECT m.entity_id
+            FROM node__field_model m
+            INNER JOIN node__field_member_of c ON field_member_of_target_id = m.entity_id
+            INNER JOIN media__field_media_of cm ON cm.field_media_of_target_id = c.entity_id
+            INNER JOIN media__field_media_use cmu ON cmu.entity_id = cm.entity_id
+            WHERE m.entity_id = :nid
+              AND cmu.field_media_use_target_id = :service
+              AND field_model_target_id = :paged
+            GROUP BY m.entity_id HAVING COUNT(*) = (
+              SELECT COUNT(*)
+                FROM node__field_model m
+                INNER JOIN node__field_member_of c ON field_member_of_target_id = m.entity_id
+                INNER JOIN media__field_media_of cm ON cm.field_media_of_target_id = c.entity_id
+                INNER JOIN media__field_media_use cmu ON cmu.entity_id = cm.entity_id
+                WHERE m.entity_id = :nid
+                  AND cmu.field_media_use_target_id = :original
+                  AND field_model_target_id = :paged
+            )
+            LIMIT 1', [
+              ':nid' => $parent->entity->id(),
+              ':service' => lehigh_islandora_get_tid_by_name('Service File', 'islandora_media_use'),
+              ':paged' => lehigh_islandora_get_tid_by_name('Paged Content', 'islandora_models'),
+          ])->fetchField();
+          if ($readyToAggregate) {
+            $action_storage = \Drupal::entityTypeManager()->getStorage('action');
+            $action = $action_storage->load('paged_content_created_aggregated_pdf');
+            $action->execute([$parent->entity]);
+          }
         }
       }
-      elseif ($node->hasField('field_model') &&
-        $node->field_model->isEmpty() &&
-        !is_null($node->field_model->entity)) {
-        switch($media->field_media_use->entity->field_external_uri->uri) {
-          case 'http://id.loc.gov/ontologies/bibframe/part':
-            // todo: check if paged content parent has all children
-            break;
-          case 'http://vocab.getty.edu/page/aat/300027363':
-          // todo: if zip, parse directory tree and create manifest
-          break;
-        }
-      }
+
       if ($action_name !== '') {
         $action_storage = \Drupal::entityTypeManager()->getStorage('action');
         $action = $action_storage->load($action_name);
         $action->execute([$node]);
       }
+      $action_name = '';
     }
   }
 }
